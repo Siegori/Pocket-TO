6470dbb7c1799b7f407583d52d415f690d76bd1b
Siegori <wambach.kealin73@gmail.com>
2023-08-04 22:34:25 +0200

    Implemented Sphinx

--- pocket-to-app-master/docs/conf.py
+++ pocket-to-app-master/docs/conf.py
@@ -20,7 +20,9 @@ release = '1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
